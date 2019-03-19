a26c69a0656156c919838170c6ae7d13937e8cbe
marcushershberger <marcusrhershberger@gmail.com>
2019-03-19 08:45:56 -0400

    Added security questions

--- signup.php
+++ signup.php
@@ -1,10 +1,10 @@
 <?php
     include('inc/conn.php');
     include('inc/php_to_html_functions.php');
-    $connection = new mysqli($db_host,$db_username, $db_password, $db_name);
+    $connection = new mysqli($db_host, $db_username, $db_password, $db_name);
     
-    if ($conn->connect_error) {
-        die("Connection failed.";
+    if ($connection->connect_error) {
+        die("Connection failed.");
     }
     
     $sql = "SELECT id, question FROM security_questions";
@@ -17,52 +17,55 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <script type="text/javascript" src="main.js"></script>
-    <title>MJM</title>
-</head>
-<body onload="populateStateDropdown()">
-    <form action="store_user.php" method="post" id="infoForm">
-        <input type="text" name="fname" placeholder="First Name"><br>
-        <input type="text" name="lname" placeholder="Last Name"><br>
-        <input type="text" name="user" placeholder="Username"><br>
-        <input type="text" name="email" placeholder="Email Address"><br>
-        <input type="password" name="pass" placeholder="Password"><br>
-        <input type="password" name="passVerif" placeholder="Retype Password"><br>
-        <input type="text" name="street" placeholder="Street"><br>
-        <input type="text" name="street2" placeholder="Street2 "><br>
-        <input type="text" name="city" placeholder="City"><br>
-        <select id="state" name="state" form="infoForm">
-            <option value="" disabled selected>State</option>
-        </select><br>
-        <input type="number" name="zip" placeholder="ZIP"><br>
-        <input type="text" name="key" placeholder="Registration Key"><br>
-	<p>Answer 3 security questions (answers are not case sensitive)</p><br>
-	<?php
-        echo "<select>";
-        echo "<option value='' disabled selected>Select a security question...</option>";
-        for ($i = 0; $i < count($questions); $i++) {
-            echo option($questions[$i]["question"], $questions[$i]["id"]);
-        }
-	?>
-	<input type="text" name="sec_ans_1" placeholder="Answer"><br>
-	<?php
-        echo "<select>";
-        echo "<option value='' disabled selected>Select a security question...</option>";
-        for ($i = 0; $i < count($questions); $i++) {
-            echo option($questions[$i]["question"], $questions[$i]["id"]);
-        }
-	?>
-	<input type="text" name="sec_ans_2" placeholder="Answer"><br>
-	<?php
-        echo "<select>";
-        echo "<option value='' disabled selected>Select a security question...</option>";
-        for ($i = 0; $i < count($questions); $i++) {
-            echo option($questions[$i]["question"], $questions[$i]["id"]);
-        }
-	?>
-	<input type="text" name="sec_ans_3" placeholder="Answer"><br>
-        <input type="submit" value="Sign Up">
-    </form>
-</body>
+    <head>
+        <script type="text/javascript" src="main.js"></script>
+        <title>MJM</title>
+    </head>
+    <body onload="populateStateDropdown()">
+        <form action="store_user.php" method="post" id="infoForm">
+            <input type="text" name="fname" placeholder="First Name"><br>
+            <input type="text" name="lname" placeholder="Last Name"><br>
+            <input type="text" name="user" placeholder="Username"><br>
+            <input type="text" name="email" placeholder="Email Address"><br>
+            <input type="password" name="pass" placeholder="Password"><br>
+            <input type="password" name="passVerif" placeholder="Retype Password"><br>
+            <input type="text" name="street" placeholder="Street"><br>
+            <input type="text" name="street2" placeholder="Street2 "><br>
+            <input type="text" name="city" placeholder="City"><br>
+            <select id="state" name="state" form="infoForm">
+                <option value="" disabled selected>State</option>
+            </select><br>
+            <input type="number" name="zip" placeholder="ZIP"><br>
+            <input type="text" name="key" placeholder="Registration Key"><br>
+        <p>Answer 3 security questions (answers are not case sensitive)</p><br>
+        <?php
+            echo "<select  id='1' onchange='removeSelections(this.value, this.id)'>";
+            echo "<option value='' disabled selected>Select a security question...</option>";
+            for ($i = 0; $i < count($questions); $i++) {
+                echo option($questions[$i]["question"], $questions[$i]["id"]);
+            }
+            echo "</select><br>";
+        ?>
+        <input type="text" name="sec_ans_1" placeholder="Answer"><br>
+        <?php
+            echo "<select  id='2' onchange='removeSelections(this.value, this.id)'>";
+            echo "<option value='' disabled selected>Select a security question...</option>";
+            for ($i = 0; $i < count($questions); $i++) {
+                echo option($questions[$i]["question"], $questions[$i]["id"]);
+            }
+            echo "</select><br>";
+        ?>
+        <input type="text" name="sec_ans_2" placeholder="Answer"><br>
+        <?php
+            echo "<select  id='3' onchange='removeSelections(this.value, this.id)'>";
+            echo "<option value='' disabled selected>Select a security question...</option>";
+            for ($i = 0; $i < count($questions); $i++) {
+                echo option($questions[$i]["question"], $questions[$i]["id"]);
+            }
+            echo "</select><br>";
+        ?>
+        <input type="text" name="sec_ans_3" placeholder="Answer"><br>
+            <input type="submit" value="Sign Up">
+        </form>
+    </body>
 </html>
