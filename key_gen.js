b96c5112850e94c54f4a636c28f697463afa407e
Marcus Hershberger <marcusrhershberger@gmail.com>
2019-09-10 10:34:24 -0400

    Add files via upload

--- key_gen.js
+++ key_gen.js
@@ -9,7 +9,7 @@ function generateKeyRequest() {
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
             var return_data = request.responseText;
-            document.getElementById("keyMsg").href = "../tyler/signup.php?key=" + return_data;
+            document.getElementById("keyMsg").href = "../main/signup.php?key=" + return_data;
             document.getElementById("keyMsg").innerHTML = "Link";
             document.getElementById("key").innerHTML = return_data;
         }
