870e95f260289c74302fbae80d43ccce4d7ff4f9
marcushershberger <marcusrhershberger@gmail.com>
2019-09-12 09:36:11 -0400

    Changed app password for gmail account

--- inc/mail_vars.php
+++ inc/mail_vars.php
@@ -2,6 +2,6 @@
 
 	$myEmail = "testing.mjm.services@gmail.com";
 
-    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))->setUsername('testing.mjm.services@gmail.com')->setPassword('jrsdpsojwwmxxqrb');
+    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))->setUsername('testing.mjm.services@gmail.com')->setPassword('hekbqebrdrpfojtn');
 
     $mailer = new Swift_Mailer($transport);
