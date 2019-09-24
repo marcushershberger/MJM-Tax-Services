f2462589390afb0eef4d081379d9405af773aa74
marcushershberger <marcusrhershberger@gmail.com>
2019-09-24 12:45:15 -0400

    Change session variable from username to id

--- db/auth_user.php
+++ db/auth_user.php
@@ -12,16 +12,16 @@
 	$username = $_POST["user"];
 	$password = $_POST["pass"];
 
-	$authUser = $connection->prepare("SELECT password_hash FROM users WHERE username = ?");
+	$authUser = $connection->prepare("SELECT id, password_hash FROM users WHERE username = ?");
 	$authUser->bind_param('s', $username);
 	$authUser->execute();
-	$authUser->bind_result($hash);
+	$authUser->bind_result($id,$hash);
 	$authUser->fetch();
     $loggedin = password_verify($password, $hash);
     session_start();
     session_regenerate_id();
     if ($loggedin) {
-        $_SESSION['USER'] = $username;
+        $_SESSION['USER'] = $id;
     }
     else {
         $_SESSION['USER'] = "";
