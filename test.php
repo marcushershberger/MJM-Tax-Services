d26ab02ecb72cd646fc5b2334f1afee97168e38b
Tyler Snodderly <snodderlyT597@outlook.com>
2019-10-14 13:26:31 -0400

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        <form method="POST" enctype="multipart/form-data" action="testh.php">
-            <input type="file" name="foo" value=""/>
-            <input type="submit" value="Upload File"/>
-        </form>
-    </body>
-</html
