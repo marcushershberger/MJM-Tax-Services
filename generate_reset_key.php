ec3eff4634db4bbd6c0d6f2e5d3eff181d3a6fb3
Tyler Snodderly <snodderlyT597@outlook.com>
2019-10-14 13:22:55 -0400

    Add files via upload

---
+++ generate_reset_key.php
@@ -0,0 +1,49 @@
+<?php
+/*
+MJM Tax Services
+This software is a management system for tax-related documents, used by tax consultants and their clients.
+Copyright (C) 2019 Marcus Hershberger and Tyler Snodderly
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+$errorMsg = "";
+if (isset($_GET['errorCode'])) {
+    $err = $_GET['errorCode'];
+    if ($err == 1) {
+        $errorMsg = "Key is not valid";
+    }
+    else if ($err == 2) {
+        $errorMsg = "Email is not valid";
+    }
+    else {
+        $errorMsg = "Key and Email are not valid";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="../key_gen.js"></script>
+    <title>MJM</title>
+</head>
+<body>
+<form action="key_generator_reset.php" method="post">
+    <input type="text" id="email" name="email" placeholder="Email"><br>
+    <p id='error'><?php echo $errorMsg; ?></p>
+    <input type="submit" value="Email Key" id="emailBtn">
+</form>
+</body>
+</html>
---
+++ reset_pass.php
@@ -0,0 +1,41 @@
+<?php
+/*
+MJM Tax Services
+This software is a management system for tax-related documents, used by tax consultants and their clients.
+Copyright (C) 2019 Marcus Hershberger and Tyler Snodderly
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+$reset_key = $_GET['key'];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="main.js"></script>
+    <title>MJM</title>
+</head>
+
+<form action="db/update_user_pass.php" method="post" id="infoForm">
+    <input type="text" name="key" placeholder="Registration Key" value="<?php echo $reset_key ?>" required><br>
+    <input type="password" id="pass" name="pass" placeholder="Password" onkeyup="comparePassword()" required><br>
+    <input type="password" id="passVerif" name="passVerif" placeholder="Retype Password" onkeyup="comparePassword()" required><br>
+    <p>Password must contain: 1 lowercase, 1 uppercase, 1 number, one special character, must be 8 characters long</p><br>
+    <input type="checkbox" onchange="showPass()" id="box"> Show Password<br>
+
+    <input type="submit" id="submit" value="Reset">
+</form>
+</body>
+</html>
